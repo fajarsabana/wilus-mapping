b139f39b369854189ede21ee1d70f9899b0cf1d9
fajarsabana <fajar.sabana@gmail.com>
2025-03-04 10:34:35 +0700

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,16 +1,35 @@
-// Initialize the map
 var map = L.map('map', {
-  center: [3.666854, 98.66797],
-  zoom: 12,
-  scrollWheelZoom: true, // Enable scroll zoom
-  zoomControl: true // Ensure zoom controls are visible
+    center: [3.666854, 98.66797],
+    zoom: 12,
+    scrollWheelZoom: true,
+    zoomControl: true
 });
 
 // Load OpenStreetMap tiles
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// Ensure UI elements remain on top
 map.getContainer().style.zIndex = "0";
 
+let geojsonLayer;
+let geojsonData = { "type": "FeatureCollection", "features": [] };
+
+function updateMap(filteredData) {
+    if (geojsonLayer) {
+        map.removeLayer(geojsonLayer);
+    }
+    geojsonLayer = L.geoJSON(filteredData, {
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup(`<b>${feature.properties.name}</b><br>${feature.properties.company}`);
+        }
+    }).addTo(map);
+}
+
+function applyFilters() {
+    const selectedCompanies = [...document.querySelectorAll('#company-filters input:checked')].map(el => el.value);
+    const filteredData = geojsonData.features.filter(feature => selectedCompanies.includes(feature.properties.company));
+    updateMap({ "type": "FeatureCollection", "features": filteredData });
+}
+
+document.getElementById("company-filters").addEventListener("change", applyFilters);
