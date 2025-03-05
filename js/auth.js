fe81b77e0996d3547b6fc4db8204908c4af88ed6
fajarsabana <fajar.sabana@gmail.com>
2025-03-05 10:45:54 +0700

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,5 +1,12 @@
-import { supabase } from "./config.js";  
+import { supabase } from "./config.js";
 
 document.addEventListener("DOMContentLoaded", function () {
     checkAuthentication();
 });
+
+async function checkAuthentication() {
+    const { data: { user } } = await supabase.auth.getUser();
+    if (!user) {
+        window.location.href = "login.html";  // Redirects to login if not authenticated
+    }
+}
