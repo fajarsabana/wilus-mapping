17789dd5ad2d451b2da942c3c1a69866de1e6620
fajarsabana <fajar.sabana@gmail.com>
2025-03-05 10:42:58 +0700

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -3,25 +3,3 @@ import { supabase } from "./config.js";
 document.addEventListener("DOMContentLoaded", function () {
     checkAuthentication();
 });
-
-async function loadData() {
-    try {
-        const response = await fetch(`${supabaseUrl}/rest/v1/wilus_mapping?select=UID,Nama Lokasi,Pemegang Wilus,geom`, {
-            headers: {
-                "apikey": supabaseKey,
-                "Authorization": `Bearer ${supabaseKey}`,
-                "Content-Type": "application/json"
-            }
-        });
-
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-
-        const data = await response.json();
-        console.log("Supabase API Response:", data);
-        loadGeoJSON(data);
-    } catch (error) {
-        console.error("Error loading data:", error);
-    }
-}
