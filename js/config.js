f51cf0814165adb37500643802862a6b87a9aa5c
fajarsabana <fajar.sabana@gmail.com>
2025-03-05 10:07:37 +0700

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,5 @@
+const supabaseUrl = "https://jqueqchgsazhompvfifr.supabase.co";
+const supabaseKey = "YOUR_SUPABASE_KEY";
+const supabase = supabase.createClient(supabaseUrl, supabaseKey);
+
+export { supabase };
