020c324a9a1c8324e8b7c012e74d55e0ca9b6a28
fajarsabana <fajar.sabana@gmail.com>
2025-03-05 10:25:21 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,6 @@
-export const supabaseUrl = "https://jqueqchgsazhompvfifr.supabase.co";
-export const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpxdWVxY2hnc2F6aG9tcHZmaWZyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA5NzM5MDIsImV4cCI6MjA1NjU0OTkwMn0.8q1m-jIL4kRgck4pwDfOYFHgFMSg2BIfBSTgIWBc_PE"; 
-export const supabase = supabase.createClient(supabaseUrl, supabaseKey);
+// config.js
+const supabaseUrl = "https://jqueqchgsazhompvfifr.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpxdWVxY2hnc2F6aG9tcHZmaWZyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA5NzM5MDIsImV4cCI6MjA1NjU0OTkwMn0.8q1m-jIL4kRgck4pwDfOYFHgFMSg2BIfBSTgIWBc_PE";
+const supabase = supabase.createClient(supabaseUrl, supabaseKey);
+
+export { supabase };
