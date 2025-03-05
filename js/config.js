6adcebe0877949e00384785d6cf61950c175ec8a
fajarsabana <fajar.sabana@gmail.com>
2025-03-05 10:37:10 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,7 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
 const supabaseUrl = "https://jqueqchgsazhompvfifr.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpxdWVxY2hnc2F6aG9tcHZmaWZyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA5NzM5MDIsImV4cCI6MjA1NjU0OTkwMn0.8q1m-jIL4kRgck4pwDfOYFHgFMSg2BIfBSTgIWBc_PE";  // Replace with a secure method in production
-const supabase = supabase.createClient(supabaseUrl, supabaseKey);
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpxdWVxY2hnc2F6aG9tcHZmaWZyIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0MDk3MzkwMiwiZXhwIjoyMDU2NTQ5OTAyfQ.7ZjSzjdlpRNObS78AU3oUZPaEzKZRUXD0hU0dUYlsx4";  // Replace with secure method in production
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 export { supabase };
-
