946cbc532a7ef291894cc68a5a08c36e5a79bb5a
fajarsabana <fajar.sabana@gmail.com>
2025-03-05 10:51:19 +0700

    Delete js/config.js

--- js/config.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
-
-const supabaseUrl = "https://jqueqchgsazhompvfifr.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpxdWVxY2hnc2F6aG9tcHZmaWZyIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0MDk3MzkwMiwiZXhwIjoyMDU2NTQ5OTAyfQ.7ZjSzjdlpRNObS78AU3oUZPaEzKZRUXD0hU0dUYlsx4";  // Replace with secure method in production
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export { supabase };
