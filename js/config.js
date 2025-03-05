148d450936f94ec3c87821295848d720fb434495
fajarsabana <fajar.sabana@gmail.com>
2025-03-05 10:23:35 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,3 @@
-const supabaseUrl = "https://jqueqchgsazhompvfifr.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpxdWVxY2hnc2F6aG9tcHZmaWZyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA5NzM5MDIsImV4cCI6MjA1NjU0OTkwMn0.8q1m-jIL4kRgck4pwDfOYFHgFMSg2BIfBSTgIWBc_PE";
-const supabase = supabase.createClient(supabaseUrl, supabaseKey);
-
-export { supabase };
+export const supabaseUrl = "https://jqueqchgsazhompvfifr.supabase.co";
+export const supabaseKey = "YOUR_SUPABASE_KEY"; 
+export const supabase = supabase.createClient(supabaseUrl, supabaseKey);
