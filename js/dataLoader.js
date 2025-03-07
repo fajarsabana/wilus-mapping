cf1f091a8bb1c0a6e28d5da54813a740cf88a61a
fajarsabana <fajar.sabana@gmail.com>
2025-03-07 11:23:46 +0700

    Update dataLoader.js

--- js/dataLoader.js
+++ js/dataLoader.js
@@ -1,14 +1,14 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 const SUPABASE_URL = "https://jqueqchgsazhompvfifr.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpxdWVxY2hnc2F6aG9tcHZmaWZyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA5NzM5MDIsImV4cCI6MjA1NjU0OTkwMn0.8q1m-jIL4kRgck4pwDfOYFHgFMSg2BIfBSTgIWBc_PE"; // Replace with your actual key
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpxdWVxY2hnc2F6aG9tcHZmaWZyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA5NzM5MDIsImV4cCI6MjA1NjU0OTkwMn0.8q1m-jIL4kRgck4pwDfOYFHgFMSg2BIfBSTgIWBc_PE"; // Use environment variable
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 export async function loadGeoJSON() {
     try {
         const { data, error } = await supabase
-            .from("wilus_mapping") // Ensure this matches your table name
+            .from("wilus_mapping")
             .select("id, UID, geom, \"Pemegang Wilus\", \"Nama Lokasi\"");
 
         if (error) throw error;
